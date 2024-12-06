f733265537ceeeedcf91f61ba832205630c12626
Eduardo Elias <eduardo.eliasap@gmail.com>
2024-12-06 10:16:42 -0300

    creating profile and post box

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/eduardoeliasap.png" />
+          <div className={styles.authorInfo}>
+            <strong>Eduardo Elias</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="05 de dezembro às 16:45h" dateTime="2024-12-05 16:45:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">eduardo.elias/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
